4f4a0ecb813b9d89ed944ccc4d9ca73022f6efc1
devmullets <devmullets@gmail.com>
2020-03-25 11:10:56 -0400

    index / show / new / create controller methods for restaurant, created index / new / show pages for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,39 @@
 class RestaurantsController < ApplicationController
+    def index
+        @restaurants = Restaurant.all
+    end 
 
+    def show
+        @restaurant = Restaurant.find(params[:id])
+    end 
+
+    def new
+        @restaurant = Restaurant.new
+    end
+
+    # def edit
+    # end
+
+    def create
+        # render plain: params[:restaurant].inspect
+
+        @restaurant = Restaurant.create(restaurant_params)
+        
+        if @restaurant.save
+            redirect_to @restaurant
+        else 
+            render 'new'
+        end 
+    end 
+
+    # def update
+    # end
+
+    # def destroy
+    # end
+
+    private
+        def restaurant_params
+            params.require(:restaurant).permit(:name, :address, :phone, :website)
+        end
 end 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
+    validates :name, :website, :address, presence: true
+    validates :phone, presence: true, length: { minimum: 9 }
     
 end
